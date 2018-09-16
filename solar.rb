dbaf4b458255520bc7c559aaae54efda8119f060
yohanis-ke <yohanes.ke@gmail.com>
2018-09-15 22:30:23 -0400

    the moon class has created

--- solar.rb
+++ solar.rb
@@ -30,5 +30,43 @@ class Body
   def mass_return
     @mass
   end
+end
 
+class Planet < Body
+  def initialize(day, year, name, mass, solar)
+    super(name, mass)
+    @day = day
+    @year = year
+
+  end
+  def self.create(day, year, name, mass, solar)
+    new_body = self.new(day, year, name, mass, solar)
+    solar.add(new_body)
+    return new_body
+  end
+end
+
+class Star < Body
+  def initialize(type, name, mass, solar)
+    super(name, mass)
+    @type = type
+  end
+  def self.create(type, name, mass, solar)
+    new_body = Star.new(type, name, mass, solar)
+    solar.add(new_body)
+    return new_body
+  end
+end
+
+class Moon < Body
+  def initialize(month, planet, name, mass, solar)
+    super(name, mass)
+    @month = month
+    @planet = planet
+  end
+  def self.create(month, planet, name, mass, solar)
+    new_body = Moon.new(month, planet, name, mass, solar)
+    solar.add(new_body)
+    return new_body
+  end
 end
