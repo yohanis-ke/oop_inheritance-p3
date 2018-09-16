6da39b46721fa0255feb600312b7447edf0fd743
yohanis-ke <yohanes.ke@gmail.com>
2018-09-15 22:16:50 -0400

    the class system and methode add & total_mass method has done

---
+++ solar.rb
@@ -0,0 +1,22 @@
+class System
+  @@bodies = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add(body)
+    @@bodies << body
+  end
+
+  def total_mass
+      sum = 0
+      @@bodies.each do |element|
+        sum += element.mass_return
+      end
+      return sum
+    end
+    def show
+        @@bodies
+    end
+end
